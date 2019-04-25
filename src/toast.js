f6c4a6d732b757f25b20068409ecf43234e324fb
Samuel Colvin <s@muelcolvin.com>
2019-04-25 14:26:40 +0100

    improving toasts

--- src/toast.js
+++ src/toast.js
@@ -2,32 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Toast, ToastHeader, ToastBody} from 'reactstrap'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {sleep} from 'reactstrap-toolbox'
 
+import {sleep} from './utils'
 import {get_create_element} from './confirm'
 
 
 class ToastMessage extends React.Component {
   state = {open: true, closing: false}
   v = 0 // used to stop the animation progression on user input, eg. new message or close
+  hover = false
+  progress_ref = React.createRef()
 
   componentDidMount () {
     this.run(this.v)
   }
 
-  componentDidUpdate (prevProps) {
-    if (this.props !== prevProps) {
-      this.v += 1
-      this.setState({open: true, closing: false})
-      this.run(this.v)
+  run = async v => {
+    const step = 20
+    const steps = this.props.time / step
+    let steps_remaining = steps
+    while (steps_remaining > 0) {
+      if (v !== this.v) {
+        return
+      }
+      if (this.progress_ref.current) {
+        this.progress_ref.current.style.width = `${steps_remaining / steps * 100}%`
+      }
+      if (!this.hover) {
+        steps_remaining -= 1
+      }
+      await sleep(step)
     }
-  }
-
-  run = async (v) => {
-    await sleep(this.props.time || 5000)
-    if (v === this.v) {
-      await this._finish(v)
-    }
+    await this._finish(v)
   }
 
   close = () => {
@@ -39,21 +45,26 @@ class ToastMessage extends React.Component {
     this.setState({closing: true})
     await sleep(700)
     if (v === this.v) {
-      this.setState({open: false, closing: false})
+      this.setState({open: false, closing: false}, () => this.props.remove())
     }
+  }
+
+  set_hover = v => {
+    this.hover = v
   }
 
   render () {
     if (!this.state.open) {
-      return null
+      return <div/>
     }
     return (
       <div className={'toast-msg' + (this.state.closing ? ' t-closing' : '')}>
-        <Toast>
+        <Toast onMouseEnter={() => this.set_hover(true)} onMouseLeave={() => this.set_hover(false)}>
           <ToastHeader toggle={() => this.close()}>
             {this.props.icon && <FontAwesomeIcon icon={this.props.icon} className="mr-2"/>}
             {this.props.title}
           </ToastHeader>
+          {this.props.progress ? <div ref={this.progress_ref} className="toast-progress"/> : null}
           <ToastBody>
             {this.props.message}
           </ToastBody>
@@ -63,6 +74,47 @@ class ToastMessage extends React.Component {
   }
 }
 
-export function message_toast ({message, title, icon, time}) {
-  ReactDOM.render(<ToastMessage {...{message, title, icon, time}}/>, get_create_element('rstb-message-toast'))
+class ToastMessages extends React.Component {
+  state = {toasts: []}
+  visible = 1  // number of visible toasts
+
+  componentDidMount () {
+    const toasts = [this.props]
+    this.setState({toasts})
+  }
+
+  componentDidUpdate (prevProps) {
+    if (this.props.tkey !== prevProps.tkey) {
+      const toasts = [...this.state.toasts]
+      toasts.push(this.props)
+      this.setState({toasts})
+      this.visible += 1
+    }
+  }
+
+  remove = () => {
+    this.visible -= 1
+    if (this.visible === 0) {
+      this.setState({toasts: []})
+    }
+  }
+
+  render () {
+    return (
+      <div className="toast-msgs">
+        {this.state.toasts.map(t => (
+          <ToastMessage {...t} key={t.tkey} remove={this.remove}/>
+        ))}
+      </div>
+    )
+  }
+}
+
+const rand_int = () => Math.round(Math.random() * 100000)
+
+export function message_toast ({message, title, icon, time = 5000, progress = true}) {
+  ReactDOM.render(
+    <ToastMessages {...{message, title, icon, time, progress, tkey: rand_int()}}/>,
+    get_create_element('rstb-message-toast')
+  )
 }
