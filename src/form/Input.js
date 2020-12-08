f0daeaf15078b48697c32e4e40c4f4662714e583
Joe Preyer <joepreyer@gmail.com>
2020-12-08 15:10:46 +0200

    Add optional extraButton to toggle component

--- src/form/Input.js
+++ src/form/Input.js
@@ -12,18 +12,15 @@ import {
 import {as_title} from '../utils'
 import Recaptcha from '../Recaptcha'
 
-export const InputLabel = ({field, children}) => (
+export const InputLabel = ({field, children}) =>
   field.show_label !== false ? (
     <BsLabel htmlFor={field.name} className={field.required ? 'required' : ''}>
-     { children}
+      {children}
       {field.title}
     </BsLabel>
   ) : null
-)
 
-export const InputHelpText = ({field}) => (
-  field.help_text ? <FormText>{field.help_text}</FormText> : null
-)
+export const InputHelpText = ({field}) => (field.help_text ? <FormText>{field.help_text}</FormText> : null)
 
 const HiddenInput = ({field, disabled, error, value, onChange, onBlur}) => (
   <BsInput
@@ -50,49 +47,53 @@ const placeholder = field => {
 
 export const InputGeneral = ({className, field, error, disabled, value, onChange, onBlur, custom_type, ...extra}) => (
   <FormGroup className={className || field.className}>
-    <InputLabel field={field}/>
-    <BsInput type={custom_type || field.type || 'text'}
-             className={field.inputClassName}
-             invalid={!!error}
-             disabled={disabled}
-             name={field.name}
-             id={field.name}
-             required={field.required}
-             maxLength={field.max_length}
-             minLength={field.min_length}
-             autoComplete={field.autocomplete}
-             placeholder={placeholder(field)}
-             value={value || ''}
-             onChange={e => onChange(e.target.value)}
-             onBlur={e => onBlur(e.target.value)}
-             {...extra}/>
+    <InputLabel field={field} />
+    <BsInput
+      type={custom_type || field.type || 'text'}
+      className={field.inputClassName}
+      invalid={!!error}
+      disabled={disabled}
+      name={field.name}
+      id={field.name}
+      required={field.required}
+      maxLength={field.max_length}
+      minLength={field.min_length}
+      autoComplete={field.autocomplete}
+      placeholder={placeholder(field)}
+      value={value || ''}
+      onChange={e => onChange(e.target.value)}
+      onBlur={e => onBlur(e.target.value)}
+      {...extra}
+    />
     {error && <FormFeedback>{error}</FormFeedback>}
-    <InputHelpText field={field}/>
+    <InputHelpText field={field} />
   </FormGroup>
 )
 
 export const InputCheckbox = ({className, field, disabled, value, onChange, onBlur}) => (
   <FormGroup className={className || field.className || 'py-2'} check>
     <InputLabel field={field}>
-      <BsInput type="checkbox"
-               label={field.title}
-               disabled={disabled}
-               name={field.name}
-               id={field.name}
-               required={field.required}
-               checked={value || false}
-               onChange={e => onChange(e.target.checked)}
-               onBlur={e => onBlur(e.target.checked)}/>
+      <BsInput
+        type="checkbox"
+        label={field.title}
+        disabled={disabled}
+        name={field.name}
+        id={field.name}
+        required={field.required}
+        checked={value || false}
+        onChange={e => onChange(e.target.checked)}
+        onBlur={e => onBlur(e.target.checked)}
+      />
     </InputLabel>
-    <InputHelpText field={field}/>
+    <InputHelpText field={field} />
   </FormGroup>
 )
 
-function prep_choice (c) {
-  if (typeof(c) === 'string') {
+function prep_choice(c) {
+  if (typeof c === 'string') {
     c = {value: c}
   }
-  if (typeof(c) !== 'object' || typeof(c.value) !== 'string') {
+  if (typeof c !== 'object' || typeof c.value !== 'string') {
     throw TypeError(`choice not an object or choice.value not a string: ${c}`)
   }
   return {value: c.value, label: c.label || as_title(c.value)}
@@ -100,92 +101,127 @@ function prep_choice (c) {
 
 export const InputSelect = ({className, field, disabled, error, value, onChange, onBlur}) => (
   <FormGroup className={className || field.className}>
-    <InputLabel field={field}/>
-    <CustomInput type="select"
-                 invalid={!!error}
-                 value={value || ''}
-                 disabled={disabled}
-                 name={field.name}
-                 id={field.name}
-                 required={field.required}
-                 autoComplete={field.autocomplete}
-                 onChange={e => onChange(e.target.value)}
-                 onBlur={e => onBlur(e.target.value)}>
+    <InputLabel field={field} />
+    <CustomInput
+      type="select"
+      invalid={!!error}
+      value={value || ''}
+      disabled={disabled}
+      name={field.name}
+      id={field.name}
+      required={field.required}
+      autoComplete={field.autocomplete}
+      onChange={e => onChange(e.target.value)}
+      onBlur={e => onBlur(e.target.value)}
+    >
       {field.allow_empty !== false && <option value="">&mdash;</option>}
-      {field.choices && field.choices.map(prep_choice).map((c, i) => (
-        <option key={i} value={c.value}>{c.label}</option>
-      ))}
+      {field.choices &&
+        field.choices.map(prep_choice).map((c, i) => (
+          <option key={i} value={c.value}>
+            {c.label}
+          </option>
+        ))}
     </CustomInput>
     {error && <FormFeedback>{error}</FormFeedback>}
-    <InputHelpText field={field}/>
+    <InputHelpText field={field} />
   </FormGroup>
 )
 
 export const InputRadio = ({className, field, disabled, error, value, onChange, onBlur}) => (
   <FormGroup className={className || field.className}>
-    <InputLabel field={field}/>
-    {field.choices && field.choices.map(prep_choice).map((c, i) => (
-      <div key={i} className="custom-control custom-radio">
-        <input type="radio"
-               className="custom-control-input"
-               checked={c.value === value}
-               disabled={disabled}
-               name={field.name}
-               id={`${field.name}-${c.value}`}
-               required={field.required}
-               onChange={() => onChange(c.value)}
-               onBlur={() => onBlur(c.value)}/>
-        <label className="custom-control-label" htmlFor={`${field.name}-${c.value}`}>
-          <span className="d-inline-block mr-1">{c.label.title || c.label}</span>
-          {c.label.description && <small className="text-muted">{c.label.description}</small>}
-        </label>
-     </div>
-    ))}
+    <InputLabel field={field} />
+    {field.choices &&
+      field.choices.map(prep_choice).map((c, i) => (
+        <div key={i} className="custom-control custom-radio">
+          <input
+            type="radio"
+            className="custom-control-input"
+            checked={c.value === value}
+            disabled={disabled}
+            name={field.name}
+            id={`${field.name}-${c.value}`}
+            required={field.required}
+            onChange={() => onChange(c.value)}
+            onBlur={() => onBlur(c.value)}
+          />
+          <label className="custom-control-label" htmlFor={`${field.name}-${c.value}`}>
+            <span className="d-inline-block mr-1">{c.label.title || c.label}</span>
+            {c.label.description && <small className="text-muted">{c.label.description}</small>}
+          </label>
+        </div>
+      ))}
     {error && <FormFeedback className="d-block">{error}</FormFeedback>}
-    <InputHelpText field={field}/>
+    <InputHelpText field={field} />
   </FormGroup>
 )
 
-export const InputToggle = ({className, field, disabled, error, value, onChange, onBlur}) => (
+export const InputToggle = ({className, field, disabled, error, value, onChange, onBlur, extraButton}) => (
   <FormGroup className={className || field.className}>
-    <InputLabel field={field}/>
+    <InputLabel field={field} />
     <div>
       <ButtonGroup>
-        {field.choices && field.choices.map(prep_choice).map((c, i) => (
-          <Button key={i} disabled={disabled} onClick={() => onChange(c.value)}
-                  color={c.value === value ? 'primary' : 'secondary'}>
-            {c.label}
+        {field.choices &&
+          field.choices.map(prep_choice).map((c, i) => (
+            <Button
+              key={i}
+              disabled={disabled}
+              onClick={() => onChange(c.value)}
+              color={c.value === value ? 'primary' : 'secondary'}
+            >
+              {c.label}
+            </Button>
+          ))}
+        {extraButton && (
+          <Button onClick={extraButton.onClick} color={extraButton.selected ? 'primary' : 'secondary'}>
+            {extraButton.label}
           </Button>
-        ))}
+        )}
       </ButtonGroup>
     </div>
-    <HiddenInput field={field} disabled={disabled} error={error} value={value} onChange={onChange} onBlur={onBlur}/>
+    <HiddenInput field={field} disabled={disabled} error={error} value={value} onChange={onChange} onBlur={onBlur} />
     {error && <FormFeedback>{error}</FormFeedback>}
-    <InputHelpText field={field}/>
+    <InputHelpText field={field} />
   </FormGroup>
 )
 
 export const InputInteger = props => (
-  <InputGeneral {...props} custom_type="number" step="1" min={props.field.min} max={props.field.max}
-                onChange={v => props.onChange(v ? parseInt(v) : null)}/>
+  <InputGeneral
+    {...props}
+    custom_type="number"
+    step="1"
+    min={props.field.min}
+    max={props.field.max}
+    onChange={v => props.onChange(v ? parseInt(v) : null)}
+  />
 )
 
 export const InputNumber = props => (
-  <InputGeneral {...props} custom_type="number" step={props.field.step} min={props.field.min} max={props.field.max}
-                onChange={v => props.onChange(v ? parseFloat(v) : null)}/>
+  <InputGeneral
+    {...props}
+    custom_type="number"
+    step={props.field.step}
+    min={props.field.min}
+    max={props.field.max}
+    onChange={v => props.onChange(v ? parseFloat(v) : null)}
+  />
 )
 
 // placeholder is useful here to provide partial support for safari which doesn't support date inputs
 export const InputDate = props => (
-  <InputGeneral {...props} custom_type="date" min={props.field.min} max={props.field.max}
-                placeholder={props.field.placeholder || 'dd/mm/yyyy'}/>
+  <InputGeneral
+    {...props}
+    custom_type="date"
+    min={props.field.min}
+    max={props.field.max}
+    placeholder={props.field.placeholder || 'dd/mm/yyyy'}
+  />
 )
 
 export const InputRecaptcha = ({className, field, error, onChange}) => (
   <FormGroup className={className || field.className}>
-    <Recaptcha onChange={onChange} element_id={field.name}/>
+    <Recaptcha onChange={onChange} element_id={field.name} />
     {error && <FormFeedback className="d-block">{error}</FormFeedback>}
-    <InputHelpText field={field}/>
+    <InputHelpText field={field} />
   </FormGroup>
 )
 
@@ -225,7 +261,7 @@ export const InputFile = ({className, field, error, disabled, onChange}) => {
 
   return (
     <FormGroup className={className || field.className}>
-      <InputLabel field={field}/>
+      <InputLabel field={field} />
       <div className="custom-file">
         <BsInput
           type="file"
@@ -242,7 +278,7 @@ export const InputFile = ({className, field, error, disabled, onChange}) => {
         </label>
       </div>
       {error && <FormFeedback className="d-block">{error}</FormFeedback>}
-      <InputHelpText field={field}/>
+      <InputHelpText field={field} />
     </FormGroup>
   )
 }
@@ -260,16 +296,8 @@ const INPUT_LOOKUP = {
 }
 
 export const InputWrapper = ({field, value, type_lookup, ...props}) => {
-  const InputComp = (
-    (type_lookup && type_lookup[field.type]) || INPUT_LOOKUP[field.type] || InputGeneral
-  )
+  const InputComp = (type_lookup && type_lookup[field.type]) || INPUT_LOOKUP[field.type] || InputGeneral
 
   field.title = field.title || as_title(field.name)
-  return (
-    <InputComp
-      field={field}
-      value={[null, undefined].includes(value) ? field.default : value}
-      {...props}
-    />
-  )
+  return <InputComp field={field} value={[null, undefined].includes(value) ? field.default : value} {...props} />
 }
